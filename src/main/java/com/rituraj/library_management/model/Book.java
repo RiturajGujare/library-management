40eca6fa877febe7391d17f1706dc871e5d87bfa
Rituraj Gujare <riturajgujare01@gmail.com>
2025-09-26 16:30:11 +0530

    added Book entity to the source code

---
+++ src/main/java/com/rituraj/library_management/model/Book.java
@@ -0,0 +1,48 @@
+package com.rituraj.library_management.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+
+    private LocalDate publishedDate;
+
+    @Column(nullable = false)
+    private Integer totalCopies = 0;
+
+    @Column(nullable = false)
+    private Integer availableCopies = 0;
+
+
+}
