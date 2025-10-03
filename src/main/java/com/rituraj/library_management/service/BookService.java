7f737cdc12bd7aeaded87732739046698583464d
Rituraj Gujare <riturajgujare01@gmail.com>
2025-10-03 12:19:48 +0530

    added BookService interface and BookServiceImpl class

---
+++ src/main/java/com/rituraj/library_management/service/BookService.java
@@ -0,0 +1,39 @@
+package com.rituraj.library_management.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.rituraj.library_management.model.Book;
+
+/*
+ * BookService Interface
+ * Defines the contract for book-related operations
+ */
+public interface BookService  {
+    
+    // Service methods for Book entity
+
+    // Add a new book
+    Book addBook(Book book);
+
+    // Update an existing book
+    Book updateBook(Long id, Book updatedBook);
+
+    // Find a book by its ISBN
+    Optional<Book> findBookByISBN(String isbn);
+
+    // Find a book by its ID
+    Optional<Book> findBookById(Long id);
+
+    // Get all books
+    List<Book> getAllBooks();
+
+    // Get books by author name
+    List<Book> getBookByAuthor(String author);
+
+    // Get books with titles containing a specific string (case insensitive)
+    List<Book> getBookByTitle(String title);
+
+    // Delete a book by its ID
+    void deleteBook(Long id);
+}
---
+++ src/main/java/com/rituraj/library_management/service/BookServiceImpl.java
@@ -0,0 +1,112 @@
+package com.rituraj.library_management.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.rituraj.library_management.model.Book;
+import com.rituraj.library_management.repository.BookRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+/*
+ * BookServiceImpl Class
+ * Implements the BookService interface to provide book-related operations
+ */
+
+ //service annotation indicates that this class is a service component in the Spring context
+@Service
+//transactional annotation indicates that methods in this class should be executed within a transaction
+@Transactional
+//requiredargsconstructor annotation generates a constructor with required arguments (final fields)
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService{
+
+    // Injecting BookRepository to interact with the database
+    private final BookRepository bookRepository;
+
+    // Service methods implementation
+
+    // Add a new book
+    @Override
+    public Book addBook(Book book){
+
+        // Check if a book with the same ISBN already exists
+        if(bookRepository.existsByISBN(book.getIsbn())){
+            throw new IllegalArgumentException("Book with ISBN already exists " + book.getIsbn());
+        }
+
+        // Save the new book to the repository
+        return bookRepository.save(book);
+    }
+
+    // Update an existing book
+    @Override
+    public Book updateBook(Long id, Book updatedBook){
+
+        // Find the existing book by ID and update its details
+        return bookRepository.findById(id).map(
+            existingBook -> {
+                if(bookRepository.existsByISBN(updatedBook.getIsbn()) && !existingBook.getIsbn().equals(updatedBook.getIsbn())){
+                    throw new IllegalArgumentException("Book with ISBN already exists " + updatedBook.getIsbn());
+                }
+                existingBook.setAuthor(updatedBook.getAuthor());
+                existingBook.setTitle(updatedBook.getTitle());
+                existingBook.setIsbn(updatedBook.getIsbn());
+                existingBook.setPublishedDate(updatedBook.getPublishedDate());
+                existingBook.setAvailableCopies(updatedBook.getAvailableCopies());
+                existingBook.setTotalCopies(updatedBook.getTotalCopies());
+                return bookRepository.save(existingBook);
+            }
+        ).orElseThrow(() -> new IllegalArgumentException("No Book with mentioned id " + id));
+        
+    }
+
+    // Find a book by its ISBN
+    @Override
+    public Optional<Book> findBookByISBN(String isbn){
+        return bookRepository.findByISBN(isbn);
+    }
+
+
+    // Find a book by its ID
+    @Override
+    public Optional<Book> findBookById(Long id){
+        return bookRepository.findById(id);
+    }
+
+
+    // Get all books
+    @Override
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    // Get books by author name
+    @Override
+    public List<Book> getBookByAuthor(String author){
+        return bookRepository.findByAuthor(author);
+    }
+    
+
+    // Get books with titles containing a specific string (case insensitive)
+    @Override
+    public List<Book> getBookByTitle(String title){
+        return bookRepository.findByTitleContainingIgnoreCase(title);
+    }
+
+
+    // Delete a book by its ID
+    @Override
+    public void deleteBook(Long id){
+
+        // Check if the book exists before attempting to delete
+        if(bookRepository.existsById(id)){
+            bookRepository.deleteById(id);
+        }
+
+        else throw new IllegalArgumentException("No Book with mentioned id " + id);
+    }
+}
